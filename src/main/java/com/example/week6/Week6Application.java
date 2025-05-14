5cbb087695b1aef38765cb8448c27082d7488154
onlyoneprivate <pllm098@naver.com>
2025-05-14 16:32:34 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/example/week6/Week6Application.java
@@ -0,0 +1,14 @@
+package com.example.week6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class Week6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6Application.class, args);
+    }
+
+}
